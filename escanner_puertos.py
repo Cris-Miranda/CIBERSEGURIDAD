7b2f677d4ae4ad066b8a03fa386ed413ad7de2d7
Cris Miranda <cristianmirandapadilla003@gmail.com>
2024-09-22 21:51:27 -0600

    Impresion de resultados

--- escanner_puertos.py
+++ escanner_puertos.py
@@ -3,8 +3,12 @@ import socket
 ip = input("Ingresa la direccion IP a escanear: ")
 # Definimos el rango
 from puerto in range(1,65535):
-
+# Usamos IPs version 4
+# Prorocolo tcp/IP
     sock = socket.socket(socket.AF.INET, socket.SOCK_STREAM)
     sock.settimeout(5)
-    
+# Se conecta a la ip que le damos en el puerto especificado
     result = sock.connect_ex((ip, puerto))
+
+    if result == 0:
+        print
